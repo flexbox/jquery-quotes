1c75bdb9f81870554929638aebfb4ae21fa0d421
Rudy VERGES <verges.rudy@gmail.com>
2016-03-24 13:14:09 +0100

    Quotations listed and button working

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,5 +1,24 @@
 $( document ).ready(function() {
     
-	//$('#js-content').load( "http://webcitation.fr/quotes.json" );
+	var dataUrl = "http://webcitation.fr/quotes.json";
 
+	$.getJSON( dataUrl, function( data ) {
+  		
+  		var items = [];
+  		$.each( data, function( key, val ) {
+    		items.push( "<blockquote class=\"blockquote\"><p class=\"m-b-0\">" + data[key].title + "</p><footer class=\"blockquote-footer\">" + data[key].author + "</footer></blockquote>" );
+  		});
+ 
+  		$( "<div/>", {
+    		"class": "quotations",
+    		html: items.join( "" )
+  		}).appendTo( "#js-content" );
+	});
+
+
+  $('#js-request').on('click', function(e){
+    e.preventDefault();
+    $('#js-request').hide();
+    $('#js-bio').load("bio.txt");
+  });
 });
