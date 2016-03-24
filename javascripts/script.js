142c71d3b3ab7dca82242704a3a88f402c692069
blackcataleya <elodieandrieux62100@gmail.com>
2016-03-24 14:04:51 +0100

    .LOAD()

--- javascripts/script.js
+++ javascripts/script.js
@@ -3,12 +3,23 @@ $(document).ready(function() {
         $.getJSON("http://webcitation.fr/quotes.json ", function (data) {
             var items = [];
             $.each(data, function (key, val) {
-                items.push("<li id='" + key + "'>" + data[key].title + "</li>");
+                items.push("<blockquote class=\"blockquote\"><p id='\" + key + \"'>" + data[key].title + "</p><footer class=\"blockquote-footer\">"+data[key].author+"</footer></blockquote>");
             });
             $("<ul/>", {
                 "class": "my-new-list",
                 html: items.join("")
-            }).appendTo("body");
+            }).appendTo("#content");
         });
+
+    $("#js-request").on('click',function(event) {
+        event.preventDefault();
+        $("#js-bio").load("bio.txt", function( response, status, xhr ) {
+            if ( status == "error" ) {
+                var msg = "Sorry but there was an error: ";
+                $( "#js-bio" ).html( msg + xhr.status + " " + xhr.statusText );
+            }
+        });
+    });
+
 
 });
